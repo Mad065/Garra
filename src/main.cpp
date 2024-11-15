179515d2591c0be558239eb9700ad21484fbfc3c
Mad0 <jose.antonio.pepe@icloud.com>
2024-11-15 11:28:52 -0600

    arreglando

--- src/main.cpp
+++ src/main.cpp
@@ -19,55 +19,95 @@ int pinServoEje = 14;
 int pasosAngulos = 2;
 
 int anguloServoGarra = 0;
-int anguloServoAltura = 0;
+int anguloServoAltura = 10;
 int anguloServoLejania = 0;
 int anguloServoEje = 0;
 
-void moverServo(Servo *servo, int anguloActual, int anguloObjetivo) {
-    if (servo == &servoGarra)
-    {
-        if (anguloObjetivo < 0) anguloObjetivo = 0; 
-        if (anguloObjetivo > 30) anguloObjetivo = 30;
-    }
+void moverServoAltura(int anguloActual, int anguloObjetivo) {
 
-    if (servo == &servoAltura)
-    {
-        if (anguloObjetivo < 0) anguloObjetivo = 10; 
-        if (anguloObjetivo > 180) anguloObjetivo = 100;
-    }
+    if (anguloObjetivo < 10) anguloObjetivo = 10; 
+    if (anguloObjetivo > 100) anguloObjetivo = 100;
 
-    if (servo == &servoLejania)
-    {
-        if (anguloObjetivo < 0) anguloObjetivo = 0; 
-        if (anguloObjetivo > 180) anguloObjetivo = 110;
-    }
-
-    if (servo == &servoEje)
-    {
-        if (anguloObjetivo < 0) anguloObjetivo = 0; 
-        if (anguloObjetivo > 180) anguloObjetivo = 180;
-    }
-    
     Serial.println(anguloObjetivo);
 
     if (anguloActual < anguloObjetivo) {
         for (; anguloActual < anguloObjetivo; anguloActual += pasosAngulos) {
-            servo->write(anguloActual);
+            servoAltura.write(anguloActual);
             delay(10);
         }
     } else {
         for (; anguloActual > anguloObjetivo; anguloActual -= pasosAngulos) {
-            servo->write(anguloActual);
+            servoAltura.write(anguloActual);
+            delay(10);
+        }
+    }
+}
+
+void moverServoGarra(int anguloActual, int anguloObjetivo) {
+
+    if (anguloObjetivo < 0) anguloObjetivo = 0; 
+    if (anguloObjetivo > 30) anguloObjetivo = 30;
+
+    Serial.println(anguloObjetivo);
+
+    if (anguloActual < anguloObjetivo) {
+        for (; anguloActual < anguloObjetivo; anguloActual += pasosAngulos) {
+            servoGarra.write(anguloActual);
+            delay(10);
+        }
+    } else {
+        for (; anguloActual > anguloObjetivo; anguloActual -= pasosAngulos) {
+            servoGarra.write(anguloActual);
+            delay(10);
+        }
+    }
+}
+
+void moverServoLejania(int anguloActual, int anguloObjetivo) {
+
+    if (anguloObjetivo < 0) anguloObjetivo = 0; 
+    if (anguloObjetivo > 110) anguloObjetivo = 110;
+
+    Serial.println(anguloObjetivo);
+
+    if (anguloActual < anguloObjetivo) {
+        for (; anguloActual < anguloObjetivo; anguloActual += pasosAngulos) {
+            servoLejania.write(anguloActual);
+            delay(10);
+        }
+    } else {
+        for (; anguloActual > anguloObjetivo; anguloActual -= pasosAngulos) {
+            servoLejania.write(anguloActual);
+            delay(10);
+        }
+    }
+}
+
+void moverServoEje(int anguloActual, int anguloObjetivo) {
+
+    if (anguloObjetivo < 0) anguloObjetivo = 0; 
+    if (anguloObjetivo > 180) anguloObjetivo = 180;
+
+    Serial.println(anguloObjetivo);
+
+    if (anguloActual < anguloObjetivo) {
+        for (; anguloActual < anguloObjetivo; anguloActual += pasosAngulos) {
+            servoEje.write(anguloActual);
+            delay(10);
+        }
+    } else {
+        for (; anguloActual > anguloObjetivo; anguloActual -= pasosAngulos) {
+            servoEje.write(anguloActual);
             delay(10);
         }
     }
 }
 
 void reiniciarServos() {
-    moverServo(&servoAltura, anguloServoAltura, 100);
-    moverServo(&servoEje, anguloServoEje, 60);
-    moverServo(&servoGarra, anguloServoGarra, 30);
-    moverServo(&servoLejania, anguloServoLejania, 0);
+    moverServoAltura(anguloServoAltura, 100);
+    moverServoEje(anguloServoEje, 60);
+    moverServoGarra(anguloServoGarra, 30);
+    moverServoLejania(anguloServoLejania, 0);
 }
 
 void setup() {
@@ -86,35 +126,35 @@ void loop() {
 
     // Comandos para los servos
     if (GamePad.isUpPressed()) {
-        moverServo(&servoAltura, anguloServoAltura, anguloServoAltura + 10);
+        moverServoAltura(anguloServoAltura, anguloServoAltura + 10);
         Serial.println("subiendo");
     }
     if (GamePad.isDownPressed()) {
-        moverServo(&servoAltura, anguloServoAltura, anguloServoAltura - 10);
+        moverServoAltura(anguloServoAltura, anguloServoAltura - 10);
         Serial.println("bajando");
     }
     if (GamePad.isRightPressed()) {
-        moverServo(&servoEje, anguloServoEje, anguloServoEje + 10);
+        moverServoEje(anguloServoEje, anguloServoEje + 10);
         Serial.println("derecha");
     }
     if (GamePad.isLeftPressed()) {
-        moverServo(&servoEje, anguloServoEje, anguloServoEje - 10);
+        moverServoEje(anguloServoEje, anguloServoEje - 10);
         Serial.println("izquierda");
     }
     if (GamePad.isCirclePressed()) {
-        moverServo(&servoGarra, anguloServoGarra, anguloServoGarra + 10);
+        moverServoGarra(anguloServoGarra, anguloServoGarra + 10);
         Serial.println("abriendo");
     }
     if (GamePad.isCrossPressed()) {
-        moverServo(&servoGarra, anguloServoGarra, anguloServoGarra - 10);
+        moverServoGarra(anguloServoGarra, anguloServoGarra - 10);
         Serial.println("cerrando");
     }
     if (GamePad.isSquarePressed()) {
-        moverServo(&servoLejania, anguloServoLejania, anguloServoLejania + 10);
+        moverServoLejania(anguloServoLejania, anguloServoLejania + 10);
         Serial.println("delante");
     }
     if (GamePad.isTrianglePressed()) {
-        moverServo(&servoLejania, anguloServoLejania, anguloServoLejania - 10);
+        moverServoLejania(anguloServoLejania, anguloServoLejania - 10);
         Serial.println("atras");
     }
     if (GamePad.isStartPressed())
